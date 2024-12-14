3f71c1e357ea943316de616752b5b2d72689f7ae
AssanaliKodland <saduasan4@gmail.com>
2024-12-14 19:10:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -27,3 +27,12 @@ else:
         ru_words.append(word)
         eng_words.append(translate)
         print("Слово успешно добавлено!")
+
+def is_api_group(chat_id):
+    return chat_id == GROUP_CHAT_ID
+
+
+@bot.message_handler(func=lambda m: True, content_types=['new_chat_participant'])
+def on_user_joins(message):
+    if not is_api_group(message.chat.id):
+        return
